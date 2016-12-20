0915a7e56a940a38883b3368c58ec5e34dd3a1a4
Ryan McCoy <ryanmccoy@gmail.com>
2016-12-20 14:12:58 -0500

    Updated project to use xUnit for tests to integrate with appveyor better

--- Maybe.Tests/BloomFilterTests.cs
+++ Maybe.Tests/BloomFilterTests.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Xunit;
 
 namespace Maybe.Tests
 {
-    [TestClass]
     public class BloomFilterTests
     {
-        [TestMethod]
+        [Fact]
         public void Contains_WhenItemHasBeenAdded_ShouldReturnTrue()
         {
             var filter = new BloomFilter<int>(100, 2);
             filter.Add(42);
-            Assert.IsTrue(filter.Contains(42));
+            Assert.True(filter.Contains(42));
         }
 
-        [TestMethod]
+        [Fact]
         public void Contains_WithFreshFilter_ShouldReturnFalse()
         {
             var filter = new BloomFilter<int>(20, 1);
-            Assert.IsFalse(filter.Contains(42));
+            Assert.False(filter.Contains(42));
         }
     }
 }
