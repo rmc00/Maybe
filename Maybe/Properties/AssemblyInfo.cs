37fdb6c2657433d1378bf685cf6888e604d144d7
Ryan McCoy <ryanmccoy@gmail.com>
2016-12-21 12:24:31 -0500

    Updated nuget pack to target specific DLLs

--- Maybe/Properties/AssemblyInfo.cs
+++ Maybe/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Maybe")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A probabilistic data structure library for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Maybe.NET")]
