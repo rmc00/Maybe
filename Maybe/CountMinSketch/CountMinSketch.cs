c311f909975ea54207b9372db9d8e2f1bacc9bb5
Ryan McCoy <ryanmccoy@gmail.com>
2017-01-12 16:02:42 -0500

    Started work on count min sketch data structure

---
+++ Maybe/CountMinSketch/CountMinSketch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maybe.CountMinSketch
+{
+    // porting from Apache spark: https://github.com/apache/spark/blob/master/common/sketch/src/main/java/org/apache/spark/util/sketch/CountMinSketchImpl.java
+    //public class CountMinSketch<T> : CountMinSketchBase<T>
+    //{
+    //    private const long PRIME_MODULUS = (1L << 31) - 1;
+
+    //    private int _depth;
+    //    private int _width;
+    //    private long[][] _table;
+    //    private long[] _hashA;
+    //    private long _totalCount;
+    //    private double _epsilon;
+    //    private double _confidence;
+
+    //    public CountMinSketch(int depth, int width, int seed)
+    //    {
+    //        if(depth <= 0) {  throw new ArgumentException("Depth must be a positive integer.", nameof(depth));}
+    //        if(width <= 0) {  throw new ArgumentException("Width must be a positive integer.", nameof(width));}
+
+    //        _depth = depth;
+    //        _width = width;
+    //        _epsilon = 2d/width;
+    //        _confidence = 1 - 1/Math.Pow(2, depth);
+    //        InitTablesWith(depth, width, seed);
+    //    }
+
+    //    public CountMinSketch(double epsilon, double confidence, int seed)
+    //    {
+    //        if(epsilon <= 0d) { throw new ArgumentException("Relative error must be positive.", nameof(epsilon)); }
+    //        if(confidence <= 0d || confidence >= 1d) { throw new ArgumentException("Confidence must be greater than 0 and less than 1", nameof(confidence)); }
+
+    //        _epsilon = epsilon;
+    //        _confidence = confidence;
+    //        _width = (int) Math.Ceiling(2/epsilon);
+    //        _depth = (int) Math.Ceiling(-Math.Log(1 - confidence)/Math.Log(2));
+    //        InitTablesWith(_depth, _width, seed);
+    //    }
+
+    //    private void InitTablesWith(int depth, int width, int seed)
+    //    {
+            
+    //    }
+    //}
+}
---
+++ Maybe/CountMinSketch/CountMinSketchBase.cs
@@ -0,0 +1,64 @@
+﻿namespace Maybe.CountMinSketch
+{
+    internal abstract class CountMinSketchBase<T>
+    {
+        /// <summary>
+        /// Returns the relative error, epsilon, of this <see cref="CountMinSketchBase{T}"/>
+        /// </summary>
+        public abstract double RelativeError { get; }
+        /// <summary>
+        /// Returns the confidence, delta, of this <see cref="CountMinSketchBase{T}"/>
+        /// </summary>
+        public abstract double Confidence { get; }
+        /// <summary>
+        /// Returns the depth of this <see cref="CountMinSketchBase{T}"/>
+        /// </summary>
+        public abstract int Depth { get; }
+        /// <summary>
+        /// Returns the width of this <see cref="CountMinSketchBase{T}"/>
+        /// </summary>
+        public abstract int Width { get; }
+        /// <summary>
+        /// Returns the total count of items added to this <see cref="CountMinSketchBase{T}"/>
+        /// </summary>
+        public abstract long TotalCount { get; }
+
+        /// <summary>
+        /// Increments counters for the item
+        /// </summary>
+        /// <param name="item"></param>
+        public abstract void Add(T item);
+
+        /// <summary>
+        /// Returns the estimated frequency of the item
+        /// </summary>
+        /// <param name="item">Item for which an estimated count is desired</param>
+        /// <returns>Estimated frequency of this item in the <see cref="CountMinSketchBase{T}"/></returns>
+        public abstract long EstimateCount(T item);
+
+        /// <summary>
+        /// Merges another <see cref="CountMinSketchBase{T}"/> with this one in place. Other must have the same depth, width, and seed to be merged.
+        /// </summary>
+        /// <param name="other"><see cref="CountMinSketchBase{T}"/> to be merged with this <see cref="CountMinSketchBase{T}"/></param>
+        /// <returns></returns>
+        public abstract CountMinSketchBase<T> MergeInPlace(CountMinSketchBase<T> other);
+
+        /// <summary>
+        /// Creates a <see cref="CountMinSketchBase{T}"/> with the given depth, width, and seed
+        /// </summary>
+        /// <param name="depth">The depth of the count-min sketch. Must be positive.</param>
+        /// <param name="width">The width of the count-min sketch. Must be positive.</param>
+        /// <param name="seed">A random seed for hashing.</param>
+        /// <returns>A new <see cref="CountMinSketchBase{T}"/> created with the provided parameters</returns>
+        //public static CountMinSketchBase<T> Create(int depth, int width, int seed) => new CountMinSketch(depth, width, seed);
+
+        /// <summary>
+        /// Creates a new <see cref="CountMinSketchBase{T}"/> with given relative error (epsilon), confidence, and random seed.
+        /// </summary>
+        /// <param name="epsilon">Relative error of the sketch. Must be positive.</param>
+        /// <param name="confidence">Confidence of frequence estimations. Must be between 0 and 1</param>
+        /// <param name="seed">A random seed for hashing.</param>
+        /// <returns>A new <see cref="CountMinSketchBase{T}"/> created with the provided parameters</returns>
+        //public static CountMinSketchBase<T> Create(double epsilon, double confidence, int seed) => new CountMinSketch(epsilon, confidence, seed);
+    }
+}
