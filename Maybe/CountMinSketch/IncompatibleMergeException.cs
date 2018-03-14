b19267c3a4763ea01b40a330fc90882dc4dfb3ff
Ryan McCoy <ryanmccoy@gmail.com>
2018-03-14 16:11:52 -0400

    Trimmed some unused code from CountMinSketch incompatible merge exception class

--- Maybe/CountMinSketch/IncompatibleMergeException.cs
+++ Maybe/CountMinSketch/IncompatibleMergeException.cs
@@ -8,21 +8,9 @@ namespace Maybe.CountMinSketch
     public class IncompatibleMergeException : Exception
     {
         /// <summary>
-        /// Creates a new instance of this exception.
-        /// </summary>
-        public IncompatibleMergeException() {  }
-
-        /// <summary>
         /// Creates a new instance of this exception with a custom error message
         /// </summary>
         /// <param name="message">The message to be set on the exception</param>
         public IncompatibleMergeException(string message) : base(message) { }
-
-        /// <summary>
-        /// Creates a new instance of this exception with a custom error message and an inner exception for context.
-        /// </summary>
-        /// <param name="message">The message to be set on the exception.</param>
-        /// <param name="inner">The inner exception to be included on the exception.</param>
-        public IncompatibleMergeException(string message, Exception inner) : base(message, inner) {  }
     }
 }
